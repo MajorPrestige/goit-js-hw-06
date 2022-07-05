e5a61d3e55d5f8a42a6c025c17954304744c33ec
Denis Dashkevich <majorprestige99@gmail.com>
2022-07-05 15:28:05 +0300

    task1 done

---
+++ js/task-01.js
@@ -0,0 +1,9 @@
+const categoriesItemEl = document.querySelectorAll(".item");
+console.log("Number of categories:", categoriesItemEl.length);
+
+categoriesItemEl.forEach(el => {
+	const searchTitle = el.querySelector("h2");
+	console.log("Category: ", searchTitle.textContent);
+	const numbersOfList = el.querySelectorAll("ul > li");
+	console.log("Elements:", numbersOfList.length);
+});
