f60f508c51f0a762bc0a984d394fb1b685a85188
Denis Dashkevich <majorprestige99@gmail.com>
2022-07-05 18:20:06 +0300

    task 3 done

--- js/task-01.js
+++ js/task-01.js
@@ -3,7 +3,7 @@ console.log("Number of categories:", categoriesItemEl.length);
 
 categoriesItemEl.forEach(el => {
 	const searchTitle = el.querySelector("h2");
-	console.log("Category: ", searchTitle.textContent);
+	console.log("Category:", searchTitle.textContent);
 	const numbersOfList = el.querySelectorAll("ul > li");
 	console.log("Elements:", numbersOfList.length);
 });
--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,13 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ["Potatoes", "Mushrooms", "Garlic", "Tomatos", "Herbs", "Condiments"];
+
+const ingridientsListEl = document.querySelector("#ingredients");
+
+const ingridientsItemListEl = ingredients.map(el => {
+	const ingridientsItemEl = document.createElement("li");
+	ingridientsItemEl.classList.add("item");
+	ingridientsItemEl.textContent = el;
+
+	return ingridientsItemEl;
+});
+
+ingridientsListEl.append(...ingridientsItemListEl);
