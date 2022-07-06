a204ef2455c3f09bf1a0bdfa8974100b892729af
Denis Dashkevich <majorprestige99@gmail.com>
2022-07-06 16:01:08 +0300

    task9 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+	return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const chageColorBtnEl = document.querySelector(".change-color");
+const backgroundColorTextEl = document.querySelector(".color");
+const bodyEl = document.querySelector("body");
+
+chageColorBtnEl.addEventListener("click", () => {
+	backgroundColorTextEl.textContent = `${getRandomHexColor()}`;
+	bodyEl.style.backgroundColor = getRandomHexColor();
+});
