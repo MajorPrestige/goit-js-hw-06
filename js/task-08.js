c6584a9d2056c68b2b4507e9befd0fd640b7c117
Denis Dashkevich <majorprestige99@gmail.com>
2022-07-06 15:47:42 +0300

    task 8 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const inputEmailEl = document.querySelector('[name="email"]');
+const inputPassEl = document.querySelector('[name="password"]');
+// const submitBtnEl = document.querySelector('[type="submit"]');
+const formEl = document.querySelector(".login-form");
 
+formEl.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+	event.preventDefault();
+	const {
+		elements: { email, password },
+	} = event.currentTarget;
+
+	if (inputEmailEl.value === "" || inputPassEl.value === "") {
+		return alert("Please fill in all the fields!");
+	}
+
+	console.log(`{Name: ${email.value}, Password: ${password.value}}`);
+	event.currentTarget.reset();
+}
